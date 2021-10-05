c338fced671f4020f66a9eed763a604128320c1d
Quan-p <quanp155@gmail.com>
2021-10-05 18:07:52 -0500

    Test whether running receive attack changes hit marker in block object to true

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -8,7 +8,12 @@ beforeEach(() => {
 test('initialize gameboard', () => {
     const array = [];
     for (let i = 0; i < 100; i += 1) {
-        array.push({ hasShip: false, isShot: false });
+        array.push({ hasShip: false, isHit: false });
     }
     expect(testBoard.board).toEqual(array);
+});
+
+test('update cell to be labeled as hit', () => {
+    testBoard.receiveAttack(90);
+    expect(testBoard.board[90].isHit).toBe(true);
 });
