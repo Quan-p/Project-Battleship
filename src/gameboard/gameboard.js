66d785672ace5e7c40fe6b6b1b28d724e3e5c558
Quan-p <quanp155@gmail.com>
2021-10-05 17:56:57 -0500

    Create 100 space gameboard pushed into array

--- src/gameboard/gameboard.js
+++ src/gameboard/gameboard.js
@@ -1,5 +1,33 @@
-class gameBoard {
-    constructor(board)
+/* eslint-disable no-unused-expressions */
+class GameBoard {
+    constructor(board) {
+        this.board = board || [];
+        if (!this.board.length) this.init();
+    }
+
+    init() {
+        for (let i = 0; i < 100; i += 1) {
+            this.board.push({ hasShip: false, isShot: false });
+        }
+    }
+
+    receiveAttack(location) {
+        this.board[location].isHit = true;
+    }
+
+    checkIfAttackHit(location) {
+        // returns true for hit and false for miss
+        return this.board[location].hasShip;
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    createLocationArray(location, ship, axis) {
+        const locationArray = [];
+        for (let i = 0; i < ship.length; i += 1) {
+            (axis === 'x') ? (locationArray.push(location + i)) : (locationArray.push(location + i * 10));
+        }
+        return locationArray;
+    }
 }
 
-export default gameBoard;
+export default GameBoard;
