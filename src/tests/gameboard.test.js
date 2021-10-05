efdc235153b468e547b90c248fe6fb152637d531
Quan-p <quanp155@gmail.com>
2021-10-05 17:57:54 -0500

    Iterate through empty array to test initializing 100 space board

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,2 +1,14 @@
-import gameBoard from '../gameboard/gameboard';
+import GameBoard from '../gameboard/gameboard';
 
+let testBoard;
+beforeEach(() => {
+    testBoard = new GameBoard();
+});
+
+test('initialize gameboard', () => {
+    const array = [];
+    for (let i = 0; i < 100; i += 1) {
+        array.push({ hasShip: false, isShot: false });
+    }
+    expect(testBoard.board).toEqual(array);
+});
