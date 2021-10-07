d21aa73c75ff6147aff09d2733660d3871ad7e0b
Quan-p <quanp155@gmail.com>
2021-10-07 12:01:55 -0500

    Player class for object creation later

---
+++ src/player/player.js
@@ -0,0 +1,16 @@
+/* eslint-disable class-methods-use-this */
+import GameBoard from '../gameboard/gameboard';
+
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.ships = [];
+        this.gameboard = new GameBoard();
+    }
+
+    fireShot(location, gameboard) {
+        if (gameboard.opponentBoard()[location] === 'empty') {
+            gameboard.receiveShot(location);
+        }
+    }
+}
