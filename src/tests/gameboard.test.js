e364c89d7a071349c0de7200f3635b69687b9fd4
Quan-p <quanp155@gmail.com>
2021-10-11 11:48:27 -0500

    Imported mroe files for test

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,5 +1,7 @@
 import GameBoard from '../gameboard/gameboard';
 import shipTypes from '../ship/shipTypes';
+import Ship from '../ship/ship';
+import shipMessage from '../ship/shipMessage';
 
 let testBoard;
 beforeEach(() => {
