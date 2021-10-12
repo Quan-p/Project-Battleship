23402dc652e2a91400da29bd9309d9310b4e7a4e
Quan-p <quanp155@gmail.com>
2021-10-12 14:13:50 -0500

    Readded non working tests due to object class typo

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -9,21 +9,21 @@ test('add ships to invalid spots, expect false', () => {
     expect(testBoard.addShip(submarine, 0, -1, Direction.right)).toBe(false);
 });
 // following four tests do not work as intended
-// test('check for ship placement overflow to the right to return false', () => {
-//     expect(testBoard.addShip(submarine, 0, 7, Direction.right)).toBe(false);
-// });
+test('check for ship placement overflow to the right to return false', () => {
+    expect(testBoard.addShip(submarine, 0, 7, Direction.right)).toBe(false);
+});
 
-// test('check for ship placement overflow upwards to return false', () => {
-//     expect(testBoard.addShip(submarine, 0, 4, Direction.up)).toBe(false);
-// });
+test('check for ship placement overflow upwards to return false', () => {
+    expect(testBoard.addShip(submarine, 0, 4, Direction.up)).toBe(false);
+});
 
-// test('Add ship at position that will overflow board left, expect false', () => {
-//     expect(testBoard.addShip(submarine, 1, 1, Direction.left)).toBe(false);
-// });
+test('Add ship at position that will overflow board left, expect false', () => {
+    expect(testBoard.addShip(submarine, 1, 1, Direction.left)).toBe(false);
+});
 
-// test('Add ship at position that will overflow board down, expect false', () => {
-//     expect(testBoard.addShip(submarine, 6, 6, Direction.down)).toBe(false);
-// });
+test('Add ship at position that will overflow board down, expect false', () => {
+    expect(testBoard.addShip(submarine, 6, 6, Direction.down)).toBe(false);
+});
 // Above tests do not work
 
 test('Add ship at valid position, expect true', () => {
