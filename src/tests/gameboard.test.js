c5eb54a4f9db61e24275997ab8b4ea2669266ac8
Quan-p <quanp155@gmail.com>
2021-10-11 19:42:01 -0500

    Test for right overflow

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -13,3 +13,7 @@ beforeEach(() => {
 test('add ships to invalid spots, expect false', () => {
     expect(testBoard.addShip(carrier, -1, -1, Direction.right)).toBe(false);
 });
+
+test('check for ship placement overflow to the right to return false', () => {
+    expect(testBoard.addShip(carrier, 0, 10, Direction.right)).toBe(false);
+});
