3bcccc93701b5f6aa014873c27df1b455764e618
Quan-p <quanp155@gmail.com>
2021-10-13 13:17:43 -0500

    Set up constructor for creating ships using DOM and game controls

---
+++ src/ship/createShips.js
@@ -0,0 +1,17 @@
+import newElement from '../createElement';
+import {
+    GameMessages, BoardSpaceStatus, AttackStatus, BoardSize, Direction 
+} from './shipMessage';
+
+class ShipDom {
+    constructor(fleet) {
+        this.fleet = fleet;
+        this.sendMessage = null;
+        this.resetButton = this.resetButton.bind(this);
+        this.startGame = this.startGame.bind(this);
+        this.sendRotate = this.sendRotate.bind(this);
+        this.selectShipToPlace = this.selectShipToPlace.bind(this);
+    }
+}
+
+export default ShipDom;
