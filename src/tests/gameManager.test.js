6987cfa433d586348fd6509dd75ded92bd4fee7b
Quan-p <quanp155@gmail.com>
2021-10-26 16:05:52 -0500

    Test for a player move resulting in a miss

--- src/tests/gameManager.test.js
+++ src/tests/gameManager.test.js
@@ -2,11 +2,30 @@ import { GameMessages, AttackStatus, GameState } from '../ship/shipMessage';
 import GameManager from '../gameboard/gameManager';
 
 beforeAll(() => {
-    jest.spyOn(GameManager.prototype, 'sendPlayerMoveToDom');
+    jest.spyOn(GameManager.prototype, 'setDomPlayerMove');
 
-    jest.spyOn(GameManager.prototype, 'sendCpuMoveToDom');
+    jest.spyOn(GameManager.prototype, 'setDomCpuMove');
 });
 
 afterAll(() => {
     jest.restoreAllMocks();
+});
+
+test('Test player click at 0, 0, expect dom to update 0, 0, with miss', () => {
+    const gm = new GameManager();
+    gm.testMode = true;
+    gm.receiveMessage(GameMessages.StartGame);
+    gm.gameState = GameState.preGame;
+
+    const playerMove = {
+        target: {
+            dataset: {
+                row: 0,
+                col: 0,
+                board: 'cpu',
+            },
+        },
+    };
+    gm.clickSquare(playerMove);
+    expect(gm.setDomPlayerMove).toBeCalledWith(0, 0, AttackStatus.miss);
 });
