94753896e98f6535a53925c11adea5eb179710ad
Quan-p <quanp155@gmail.com>
2021-10-26 15:24:47 -0500

    Set before and after mock functions for tests

--- src/tests/gameManager.test.js
+++ src/tests/gameManager.test.js
@@ -1,6 +1,12 @@
-import {
-    GameMessages,
-    AttackStatus,
-    GameState,
-}
-    from '../ship/shipMessage';
+import { GameMessages, AttackStatus, GameState } from '../ship/shipMessage';
+import GameManager from '../gameboard/gameManager';
+
+beforeAll(() => {
+    jest.spyOn(GameManager.prototype, 'sendPlayerMoveToDom');
+
+    jest.spyOn(GameManager.prototype, 'sendCpuMoveToDom');
+});
+
+afterAll(() => {
+    jest.restoreAllMocks();
+});
